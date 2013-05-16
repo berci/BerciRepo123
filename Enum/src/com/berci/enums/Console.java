176770e0aefc0cec3b6661f5379050d999e2e4c0
berci <marton@marton1.camline.com>
2013-05-16 10:55:15 +0200

    update

--- Enum/src/com/berci/enums/Console.java
+++ Enum/src/com/berci/enums/Console.java
@@ -1,5 +1,49 @@
 package com.berci.enums;
 
+import java.io.PrintStream;
+
 public enum Console {
+	INFO("INFO", System.out) {
+		@Override
+		public void print(String s) {
+			ps.print(getFormattedMessage(s));
+		}
+		@Override
+		public void println(String s) {
+			ps.println(getFormattedMessage(s));
+		}
+	},
+	ERROR("ERROR", System.err) {
+		@Override
+		public void print(String s) {
+			ps.print(getFormattedMessage(s));
+		}
+		@Override
+		public void println(String s) {
+			ps.println(getFormattedMessage(s));
+		}
+	};
+	
+	String name;
+	PrintStream ps;
+	
+	private Console(String name, PrintStream ps) {
+		this.name = name;
+		this.ps = ps;
+	}
+	
+	public String getFormattedMessage(String s) {
+		String message = null;
+		message = name + " " + s;
+		return message;
+	}
+		
+	public void print(String s) {
+        throw new AbstractMethodError();
+    }
+	
+	public void println(String s) {
+        throw new AbstractMethodError();
+    }
 
 }
---
+++ Enum/src/com/berci/enums/TestConsoleMessage.java
@@ -0,0 +1,10 @@
+package com.berci.enums;
+
+public class TestConsoleMessage {
+	public static void main(String[] args) {
+		
+		Console.INFO.println("sssss");
+		
+		Console.ERROR.println("hello");
+	}
+}
